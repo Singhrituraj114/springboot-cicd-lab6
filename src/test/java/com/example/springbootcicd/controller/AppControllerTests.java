4cc9c5ab7e196e18b1d96e8a4524d469330813c5
RITURAJ SINGH (RA2211003010013) <rs9970@srmist.edu.in>
2025-10-08 02:55:38 +0530

    Fix AppControllerTests - Add @MockBean for BookService and OrderService

--- src/test/java/com/example/springbootcicd/controller/AppControllerTests.java
+++ src/test/java/com/example/springbootcicd/controller/AppControllerTests.java
@@ -1,8 +1,11 @@
 package com.example.springbootcicd.controller;
 
+import com.example.springbootcicd.service.BookService;
+import com.example.springbootcicd.service.OrderService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,6 +16,12 @@ public class AppControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @MockBean
+    private OrderService orderService;
 
     @Test
     public void testHomeEndpoint() throws Exception {
